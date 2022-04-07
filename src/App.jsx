e2b00e2955216beeb2e3aa4048e993c7760d1845
Angel Garcia Marin <isc18350309@gmail.com>
2022-04-07 16:26:15 -0500

    Agregando botón reset y resta

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,81 @@
 import { useState } from "react";
-import "./app.css"
-
-
+import "./app.css";
 
 const App = () => {
-  const [cuenta, setCuenta] = useState({valor:0, paso:1});
+  const [cuenta, setCuenta] = useState({ valor: 0, paso: 1 });
 
-  const handleClick = (e) => {
-    setCuenta({...cuenta, valor:cuenta.valor+ cuenta.paso});
-  }
-const handleChange = (e) =>{
-  const paso =  Number (e.target.value);
-  if(isNaN(paso)){
-    return;
-  }
-  setCuenta({...cuenta, paso:paso});
-};
+  const handleMinusClick = (v) => {
+    setCuenta({ ...cuenta, valor: cuenta.valor - cuenta.paso });
+  };
 
-const handleKeyDown = (e) =>{
-  const value = Number(e.key);
-  if(
-    e.key ==="Backspace" ||
-    e.key ==="Enter" ||
-    e.key ==="Delete" ||
-    e.key ==="ArrowLeft" ||
-    e.key ==="ArrowRight" ||
-    e.key ==="ArrowUp" ||
-    e.key ==="ArrowDown" ||
-    e.key ==="Tab"
-  ){
-    return;
-  }
+  const handleResetClick = (v) => {
+    setCuenta({ ...cuenta, valor: 0 });
+  };
 
-  if(isNaN(value) || e.key=== " "){
-    e.preventDefault();
-    return;
-  }
-}
+  const handlePlusClick = (v) => {
+    setCuenta({ ...cuenta, valor: cuenta.valor + cuenta.paso });
+  };
 
+  const handleChange = (e) => {
+    const paso = Number(e.target.value);
+    if (isNaN(paso)) {
+      return;
+    }
+    setCuenta({ ...cuenta, paso: Number(e.target.value) });
+  };
+
+  const handleKeyDown = (e) => {
+    const value = Number(e.key);
+    if (
+      e.key === "Backspace" ||
+      e.key === "Enter" ||
+      e.key === "Delete" ||
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowRight" ||
+      e.key === "ArrowUp" ||
+      e.key === "ArrowDown" ||
+      e.key === "Tab"
+    ) {
+      return;
+    }
+
+    if (isNaN(value) || e.key === " ") {
+      e.preventDefault();
+      return;
+    }
+  };
 
   return (
     <div className="App">
       <h1>CONTADOR</h1>
       <hr />
       <h2 className="text-center"> {cuenta.valor}</h2>
-     <div className="margin-button" >
-       <label>
-         Paso
-         <input  
-         type="text" 
-         style={{ width:"60px", margin:"5px"}} onChange = {handleChange} onKeyDown = {handleKeyDown} 
-         />
-       </label>
+      <div className="margin-button">
+        <label>
+          Paso
+          <input
+            type="text"
+            style={{ width: "60px", margin: "5px" }}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+        </label>
 
-      <button className="btn btn-primary me-2" 
-      onClick={handleClick}>+{cuenta.paso}</button>
+        <button className="btn btn-primary me-2" onClick={handleMinusClick}>
+          -{cuenta.paso}
+        </button>
+
+        <button className="btn btn-primary me-2" onClick={handleResetClick}>
+          reset
+        </button>
+
+        <button className="btn btn-primary me-2" onClick={handlePlusClick}>
+          {" "}
+          +{cuenta.paso}
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
